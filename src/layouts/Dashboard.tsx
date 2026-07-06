13c598be2fb251da9f1ee66055d88c52540627ca
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-06 14:27:31 +0530

    add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,18 +1,91 @@
-import { Navigate, Outlet } from "react-router-dom"
-import { useAuthStore } from "../store"
+import { Navigate, NavLink, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store";
+import Sider from "antd/es/layout/Sider";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Icon, { UserOutlined } from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import UserIcon from "../components/icons/User";
+import { FoodIcon } from "../components/icons/FoodIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+
+const items = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to={"/"}>Home</NavLink>,
+  },
+  {
+    key: "/users",
+    icon: <Icon component={UserIcon} />,
+    label: <NavLink to={"/users"}>Users</NavLink>,
+  },
+  {
+    key: "/restaurants",
+    icon: <Icon component={FoodIcon} />,
+    label: <NavLink to={"/restaurants"}>Restaurants</NavLink>,
+  },
+  {
+    key: "/products",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to={"/products"}>Products</NavLink>,
+  },
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to={"/promos"}>Promos</NavLink>,
+  },
+];
 
 const Dashboard = () => {
-    const {user} = useAuthStore()
+  const { user } = useAuthStore();
+  const [collapsed, setCollapsed] = useState(false);
 
-    if(user === null){
-        return <Navigate to={"/auth/login"} replace={true} />
-    }
-    
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  if (user === null) {
+    return <Navigate to={"/auth/login"} replace={true} />;
+  }
+
+  const currentYear = new Date().getFullYear();
+
   return (
     <div>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Pizza Shop ©{currentYear} Created by Prashant Gupta
+          </Footer>
+        </Layout>
+      </Layout>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
