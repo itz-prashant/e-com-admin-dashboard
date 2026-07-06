e996f72e84b5eee8d95693c1822077be00e687f7
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-06 13:13:29 +0530

    add auto refresh token

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useAuthStore } from "../store"
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_API_URL,
@@ -7,4 +8,28 @@ export const api = axios.create({
         "Content-Type": "application/json",
         Accept: "application/json"
     }
+})
+
+
+const refreshToken = async ()=>{
+    axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,{}, {withCredentials: true})
+}
+
+api.interceptors.response.use((res)=> res, async(err)=>{
+    const originalRequest = err.config
+
+    if(err.response.status === 401 && !originalRequest._isRetray){
+        try {
+            originalRequest._isRetray = true
+            const headers = {...originalRequest.headers}
+            await refreshToken()
+            return api.request({...originalRequest, headers})
+        } catch (error) {
+            console.error("Token refresh error",error)
+            useAuthStore.getState().logout()
+            return Promise.reject(error)
+        }
+    }
+
+    return  Promise.reject(err)
 })
