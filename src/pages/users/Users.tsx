8bc83b514f87750d3c001f4928bec9a3ca83aeaa
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-06 21:48:56 +0530

    add users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,12 +1,59 @@
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Space, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../../http/api";
 import type { User } from "../../types";
 
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "First Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+//   {
+//     title: "Action",
+//     dataIndex: "action",
+//     key: "action",
+//     render: () => {
+//       return (
+//         <div>
+//           <Link to={"/users/edit"}>Edit</Link>
+//         </div>
+//       );
+//     },
+//   },
+];
+
 const Users = () => {
-  const { data: getUser, isLoading } = useQuery({
+  const {
+    data: getUser,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["users"],
     queryFn: () => {
       return getUsers().then((res) => res.data);
@@ -15,21 +62,16 @@ const Users = () => {
 
   return (
     <>
-      <Breadcrumb
+      <Space style={{width: "100%"}} size={"large"} vertical>
+        <Breadcrumb
         separator={<RightOutlined />}
         items={[{ title: <Link to={"/"}>Dashboard</Link> }, { title: "Users" }]}
       />
       {isLoading && <div>Loading....</div>}
-      {getUser && (
-        <div>
-          <h1>Users</h1>
-          <ul>
-            {getUser.map((user:User) => (
-              <li key={user.id}>{user.firstName}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {isError && <div>{error.message}</div>}
+
+      <Table dataSource={getUser} columns={columns} />
+      </Space>
     </>
   );
 };
