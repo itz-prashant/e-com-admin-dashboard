c25b0280cf3742d0fd65a5a77ca273bbe70d19a4
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-10 17:09:13 +0530

    hide restaurant if role is admin

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -3,7 +3,8 @@ import { getTenants } from "../../../http/api";
 import { useQuery } from "@tanstack/react-query";
 import type { Tenant } from "../../../types";
 
-const UserForm = ({isEditMode}:{isEditMode:boolean}) => {
+const UserForm = ({ isEditMode }: { isEditMode: boolean }) => {
+  const selectedRole = Form.useWatch("role");
   const { data: tenants } = useQuery({
     queryKey: ["tenants"],
     queryFn: () => {
@@ -18,68 +19,90 @@ const UserForm = ({isEditMode}:{isEditMode:boolean}) => {
           <Card title="Basic info">
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item label="First name" name="firstName" rules={[
+                <Form.Item
+                  label="First name"
+                  name="firstName"
+                  rules={[
                     {
-                        required: true,
-                        message: "First name required"
-                    }
-                ]}>
+                      required: true,
+                      message: "First name required",
+                    },
+                  ]}
+                >
                   <Input size="large" />
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Last name" name="lastName" rules={[
+                <Form.Item
+                  label="Last name"
+                  name="lastName"
+                  rules={[
                     {
-                        required: true,
-                        message: "Last name required"
-                    }
-                ]}>
+                      required: true,
+                      message: "Last name required",
+                    },
+                  ]}
+                >
                   <Input size="large" />
                 </Form.Item>
               </Col>
               <Col span={12}>
-                <Form.Item label="Email" name="email" rules={[
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
                     {
-                        required: true,
-                        message: "First name required"
+                      required: true,
+                      message: "First name required",
                     },
                     {
-                        type: "email",
-                        message: "Email is not valid"
+                      type: "email",
+                      message: "Email is not valid",
                     },
-                ]}>
+                  ]}
+                >
                   <Input size="large" />
                 </Form.Item>
               </Col>
             </Row>
           </Card>
 
-         {!isEditMode && <Card title="Security info">
-            <Row gutter={20}>
-              <Col span={12}>
-                <Form.Item label="Password" name="password" rules={[
-                    {
+          {!isEditMode && (
+            <Card title="Security info">
+              <Row gutter={20}>
+                <Col span={12}>
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                      {
                         required: true,
-                        message: "Password is required"
-                    }
-                ]}>
-                  <Input.Password size="large" />
-                </Form.Item>
-              </Col>
-            </Row>
-          </Card>}
+                        message: "Password is required",
+                      },
+                    ]}
+                  >
+                    <Input.Password size="large" />
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Card>
+          )}
 
           <Card title="Role">
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item label="Role" name="role" rules={[
+                <Form.Item
+                  label="Role"
+                  name="role"
+                  rules={[
                     {
-                        required: true,
-                        message: "Role is required"
-                    }
-                ]}>
+                      required: true,
+                      message: "Role is required",
+                    },
+                  ]}
+                >
                   <Select
-                  id="selectBoxInUserForm"
+                    id="selectBoxInUserForm"
                     size="large"
                     onChange={() => {}}
                     allowClear={true}
@@ -88,31 +111,38 @@ const UserForm = ({isEditMode}:{isEditMode:boolean}) => {
                     options={[
                       { value: "admin", label: "Admin" },
                       { value: "manager", label: "Manager" },
-                      { value: "customer", label: "Customer" },
                     ]}
                   />
                 </Form.Item>
               </Col>
-              <Col span={12}>
-                <Form.Item label="Restaurant" name="tenantId" rules={[
-                    {
+              {selectedRole === "manager" && (
+                <Col span={12}>
+                  <Form.Item
+                    label="Restaurant"
+                    name="tenantId"
+                    rules={[
+                      {
                         required: true,
-                        message: "Restaurant is required"
-                    }
-                ]}>
-                 {tenants && <Select
-                    size="large"
-                    onChange={() => {}}
-                    allowClear={true}
-                    placeholder={"Select Restaurant"}
-                    style={{ width: "100%" }}
-                    options={tenants.map((tenant:Tenant) => ({
-                      label: tenant.name,
-                      value: tenant.id,
-                    }))}
-                  />}
-                </Form.Item>
-              </Col>
+                        message: "Restaurant is required",
+                      },
+                    ]}
+                  >
+                    {tenants && (
+                      <Select
+                        size="large"
+                        onChange={() => {}}
+                        allowClear={true}
+                        placeholder={"Select Restaurant"}
+                        style={{ width: "100%" }}
+                        options={tenants.map((tenant: Tenant) => ({
+                          label: tenant.name,
+                          value: tenant.id,
+                        }))}
+                      />
+                    )}
+                  </Form.Item>
+                </Col>
+              )}
             </Row>
           </Card>
         </Space>
