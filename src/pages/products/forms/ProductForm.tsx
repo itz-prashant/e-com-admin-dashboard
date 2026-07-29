09557a6fce0ce132205787d57ada622bdd82dcf4
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-29 13:07:02 +0530

    refactor upload image component

--- src/pages/products/forms/ProductForm.tsx
+++ src/pages/products/forms/ProductForm.tsx
@@ -3,29 +3,23 @@ import {
   Col,
   Form,
   Input,
-  message,
   Row,
   Select,
   Space,
   Switch,
   Typography,
-  Upload,
-  type UploadProps,
 } from "antd";
 import { useQuery } from "@tanstack/react-query";
 import { getCategories, getTenants } from "../../../http/api";
-import { PlusOutlined } from "@ant-design/icons";
+
 import type { Category, Tenant } from "../../../types";
 import Pricing from "./Pricing";
 import Attributes from "./Attributes";
-import { useState } from "react";
+import ProductImage from "./ProductImage";
 
 const ProductForm = () => {
   const selectedCategory = Form.useWatch("categoryId");
-  const [messageApi, contextHolder] = message.useMessage();
 
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
-  const form = Form.useFormInstance();
 
   const { data: categories } = useQuery({
     queryKey: ["categories"],
@@ -40,26 +34,6 @@ const ProductForm = () => {
       return getTenants();
     },
   });
-
-  const uploaderConfig: UploadProps = {
-    name: "file",
-    multiple: false,
-    showUploadList: false,
-    beforeUpload: (file) => {
-      const isJpgOrPng =
-        file.type === "image/jpeg" || file.type === "image/png";
-
-      if (!isJpgOrPng) {
-        messageApi.error("You can upload only Jpeg or Png");
-      }
-
-      setImageUrl(URL.createObjectURL(file));
-        form.setFieldValue("image", {
-    file: file,
-  });
-      return false;
-    },
-  };
 
   return (
     <Row>
@@ -129,28 +103,7 @@ const ProductForm = () => {
           <Card title="Product Image">
             <Row gutter={20}>
               <Col span={12}>
-                <Form.Item
-                  label=""
-                  name="image"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please upload a product image",
-                    },
-                  ]}
-                >
-                  {contextHolder}
-                  <Upload listType="picture-card" {...uploaderConfig}>
-                    {imageUrl ? (
-                      <img src={imageUrl} alt="product-image" height={100} />
-                    ) : (
-                      <Space vertical>
-                        <PlusOutlined />
-                        <Typography.Text>Upload</Typography.Text>
-                      </Space>
-                    )}
-                  </Upload>
-                </Form.Item>
+                <ProductImage />
               </Col>
             </Row>
           </Card>
---
+++ src/pages/products/forms/ProductImage.tsx
@@ -0,0 +1,63 @@
+import {
+  Form,
+  message,
+  Space,
+  Typography,
+  Upload,
+  type UploadProps,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { useState } from "react";
+
+const ProductImage = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const form = Form.useFormInstance();
+
+  const uploaderConfig: UploadProps = {
+    name: "file",
+    multiple: false,
+    showUploadList: false,
+    beforeUpload: (file) => {
+      const isJpgOrPng =
+        file.type === "image/jpeg" || file.type === "image/png";
+
+      if (!isJpgOrPng) {
+        messageApi.error("You can upload only Jpeg or Png");
+      }
+
+      setImageUrl(URL.createObjectURL(file));
+      form.setFieldValue("image", {
+        file: file,
+      });
+      return false;
+    },
+  };
+  return (
+    <Form.Item
+      label=""
+      name="image"
+      rules={[
+        {
+          required: true,
+          message: "Please upload a product image",
+        },
+      ]}
+    >
+      <Upload listType="picture-card" {...uploaderConfig}>
+        {contextHolder}
+        {imageUrl ? (
+          <img src={imageUrl} alt="product-image" height={100} />
+        ) : (
+          <Space vertical>
+            <PlusOutlined />
+            <Typography.Text>Upload</Typography.Text>
+          </Space>
+        )}
+      </Upload>
+    </Form.Item>
+  );
+};
+
+export default ProductImage;
